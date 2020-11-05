7f9245f9ca01a4545c1fdd837ec7e9128f6be79d
Ramón <55927337+Ramon3210@users.noreply.github.com>
2020-11-04 22:18:16 -0600

    Update CrecimientoView.jsx

--- src/view/CrecimientoView.jsx
+++ src/view/CrecimientoView.jsx
@@ -1,1 +1,27 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Container, Button } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import ReactPlayer from 'react-player'
 
+class CrecimientoView extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="css-crecimiento">
+        <Link to="/">
+          <Button
+            variant="contained"
+            color="default"
+            size="small"
+            startIcon={<HomeIcon />}
+          >
+            Regresar
+          </Button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default CrecimientoView;
