5c54ca019e222de477a2b56b6a4e168790696940
Ramón <55927337+Ramon3210@users.noreply.github.com>
2020-11-04 22:15:46 -0600

    Update Crecimiento.jsx

--- src/component/Crecimiento.jsx
+++ src/component/Crecimiento.jsx
@@ -1,1 +1,20 @@
+import React from 'react'
+import Header from './Header';
+import CrecimientoView from '../view/CrecimientoView';
+import Footer from './Footer';
+
+class Crecimiento extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+            <Header name="Crecimiento" />
+                <CrecimientoView />
+                <Footer />
+            </div>
+         );
+    }
+}
+ 
+export default Crecimiento;
 
