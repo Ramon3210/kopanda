39876e7f2380e35716b56b42eecc5a9a96b07dcb
Ramón <55927337+Ramon3210@users.noreply.github.com>
2020-11-05 04:51:36 +0100

    Update DatosView.jsx

--- src/view/DatosView.jsx
+++ src/view/DatosView.jsx
@@ -75,7 +75,7 @@ class DatosView extends Component {
             Regresar
           </Button>
         </Link>
- <h2>Para fortalecer la producción de aguacate, será importante impulsar el consumo interno, mejorar la infraestructura logística, el transporte y la mano de obra calificada. Queremos apoyar este crecimiento y la promesa de una vida mejor para los agricultores del país.</h2>
+ <h4>Para fortalecer la producción de aguacate, será importante impulsar el consumo interno, mejorar la infraestructura logística, el transporte y la mano de obra calificada. Queremos apoyar este crecimiento y la promesa de una vida mejor para los agricultores del país.</h4>
         {/* <h1>{this.props.name}</h1>
         <form autoComplete="off" onSubmit={this.addData}>
           <table>
