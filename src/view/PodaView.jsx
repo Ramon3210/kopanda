407690dc9e579867ff59994a2c5b54ad1eefc505
Ramón <55927337+Ramon3210@users.noreply.github.com>
2020-11-07 04:49:58 +0100

    Create PodaView.jsx

---
+++ src/view/PodaView.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Container, Button } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import ReactPlayer from 'react-player'
+
+class PodaView extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="css-actividad">
+        <Link to="/">
+          <Button
+            variant="contained"
+            color="default"
+            size="small"
+            startIcon={<HomeIcon />}
+          >
+            Regresar
+          </Button>
+        </Link>
+  
+        <font size="6" face="Gabriola" color="black">Aquí va la PODA</font>
+
+      </div>
+    );
+  }
+}
+
+export default PodaView;
